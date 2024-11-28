0a560bf3c8d987e9e10b6f13fd212d16a79d91af
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-11-29 00:53:27 +0530

    build(fix): add dynamic=`force-dynamic` on `/explore` page with revalidation of cache

--- src/app/(views)/(public)/explore/page.tsx
+++ src/app/(views)/(public)/explore/page.tsx
@@ -2,11 +2,12 @@ import fetchApi from "~/utils/fetch";
 
 import { CatalogExplorer } from "./catalog-explorer";
 
+export const dynamic = "force-dynamic";
 export const revalidate = 60 * 5; // Cache the page for 5 minutes, unless revalidated on updates
 
 export default async function ExplorePage() {
   const catalogs = await fetchApi("/catalogs/valid");
-  const archives = await fetchApi("/archives/valid")
+  const archives = await fetchApi("/archives/valid");
   return (
     <div className="p-3">
       <h1 className="text-2xl font-semibold tracking-tight">Explore</h1>
