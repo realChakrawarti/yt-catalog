9b5c580ee9ebf9efca22defd25fee59bfa3b9a2c
jey <jeyshreemen.info@gmail.com>
2024-12-28 20:00:35 +0530

    chore: updated loading with skeleton loading

--- src/app/(views)/(public)/a/[archiveId]/loading.tsx
+++ src/app/(views)/(public)/a/[archiveId]/loading.tsx
@@ -1,9 +1,37 @@
-import Spinner from "~/components/custom/spinner";
+import GridContainer from "~/components/custom/grid-container";
+import { Skeleton } from "~/components/shadcn/skeleton";
 
 export default function CatalogPublicLoading() {
   return (
-    <div className="h-full flex flex-col items-center mt-7">
-      <Spinner label="Archive videos are loading." className="size-7" />
+    <div className="h-full flex flex-col mt-7 gap-10 px-10">
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <GridContainer>
+        {(() => {
+          const items = [];
+          for (let i = 0; i < 12; i++) {
+            items.push(<YouTubeSkeletonCard key={i} />);
+          }
+          return items;
+        })()}
+      </GridContainer>
     </div>
   );
+}
+
+function YouTubeSkeletonCard(){
+  return (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  )
 }
