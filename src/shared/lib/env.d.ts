0f5ec9b76267e0816360d36ddcffdbe0d4a61b35
realChakrawarti <anupam.chakrawarti@outlook.com>
2025-02-18 10:22:09 +0530

    chore: type the env variable on `env.d.ts`

---
+++ src/shared/lib/env.d.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { z } from "zod";
+
+const envVariables = z.object({
+  GOOGLE_ANALYTICS_PRIVATE_KEY: z.string(),
+  GOOGLE_ANALYTICS_PROPERTY_ID: z.string(),
+  GOOGLE_CLIENT_EMAIL: z.atring(),
+  NEXT_PUBLIC_API_BASE_URL: z.string(),
+  NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
+  OCTOKIT_API: z.string(),
+  YOUTUBE_API_KEY: z.string(),
+});
+
+envVariables.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
