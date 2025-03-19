f3e0286369f663a12aeecd58c8c33ffb91d3f746
realChakrawarti <anupam.chakrawarti@outlook.com>
2025-03-19 23:22:32 +0530

    fix: remove channel topics from catalog

--- src/entities/catalogs/services/update-catalog-channels.ts
+++ src/entities/catalogs/services/update-catalog-channels.ts
@@ -1,11 +1,9 @@
-// TODO: Remove Topic data, payload should only be array of channels, make sure to type it
 import { doc, updateDoc } from "firebase/firestore";
 
 import { YOUTUBE_CHANNELS_INFORMATION } from "~/shared/lib/api/youtube-endpoints";
 import { COLLECTION } from "~/shared/lib/firebase/collections";
 import { db } from "~/shared/lib/firebase/config";
-
-import topicData from "./youtube-topics.json";
+import { CatalogChannel } from "~/shared/types-schema/types";
 
 /**
  * This function updates the channels of a specific catalogId
@@ -35,28 +33,20 @@ export async function updateCatalogChannels(
   }
 }
 
-type TopicId = keyof typeof topicData;
-
-function extractTopics(topicIds: TopicId[]): string[] {
-  let topicNames: string[] = [];
-  topicIds.forEach((topicId) => topicNames.push(topicData[topicId]));
-  return topicNames;
-}
-
 /**
  * Retrieves detailed information for a list of YouTube channels.
  *
  * @param channels - An array of YouTube channel IDs to fetch information for
- * @returns An array of channel metadata objects with ID, handle, title, description, logo, and topics
+ * @returns An array of channel metadata objects with ID, handle, title, description and logo
  *
  * @remarks
  * Fetches channel information from the YouTube API and transforms the response into a structured format.
- * Extracts key details such as channel title, description, custom URL, thumbnail, and associated topics.
+ * Extracts key details such as channel title, description, custom URL and thumbnail.
  *
  * @throws {Error} If there's an issue fetching channel information from the YouTube API
  */
 async function getChannelsInfo(channels: string[]) {
-  let channelsInfo: any[] = [];
+  let channelsInfo: CatalogChannel[] = [];
   // Create a separate doc with channels information, fetch from youtube API
   const response = await fetch(YOUTUBE_CHANNELS_INFORMATION(channels));
   const result = await response.json();
@@ -66,17 +56,13 @@ async function getChannelsInfo(channels: string[]) {
   for (let i = 0; i < channelListItems?.length; i++) {
     const channelInfo = channelListItems[i];
 
-    const channelMeta = {
+    channelsInfo.push({
       description: channelInfo.snippet.description,
       handle: channelInfo.snippet.customUrl,
       id: channelInfo.id,
       logo: channelInfo.snippet.thumbnails.medium.url,
       title: channelInfo.snippet.title,
-      // TODO: Remove Topics, feels a bit redundant
-      topics: extractTopics(channelInfo.topicDetails.topicIds),
-    };
-
-    channelsInfo.push(channelMeta);
+    });
   }
 
   return channelsInfo;
