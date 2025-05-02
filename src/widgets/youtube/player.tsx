5b34957766755b0ead4a664261c58aaebe0b032d
realChakrawarti <anupam.chakrawarti@outlook.com>
2025-05-03 02:22:16 +0530

    feat: plays only one video at a time, stops other playing

--- src/widgets/youtube/player.tsx
+++ src/widgets/youtube/player.tsx
@@ -16,26 +16,30 @@ export default function YoutubePlayer(
   const trackingRef = useRef<NodeJS.Timeout | null>(null);
   const playerRef = useRef<YT.Player | null>(null);
 
+  function percentCompleted(node: YT.Player) {
+    const duration = node.getDuration() || 0;
+    const currentTime = node.getCurrentTime() || 0;
+    const percentCompleted = parseInt(
+      ((currentTime / duration) * 100).toFixed(0)
+    );
+
+    const payload: History = {
+      completed: percentCompleted,
+      duration: currentTime,
+      updatedAt: Date.now(),
+      ...video,
+    };
+
+    return payload;
+  }
+
   const startTracking = () => {
     if (trackingRef.current) return;
 
     trackingRef.current = setInterval(async () => {
       if (!playerRef.current) return;
 
-      const duration = playerRef.current.getDuration() || 0;
-      const currentTime = playerRef.current.getCurrentTime() || 0;
-      const percentCompleted = parseInt(
-        ((currentTime / duration) * 100).toFixed(0)
-      );
-
-      const payload: History = {
-        completed: percentCompleted,
-        duration: currentTime,
-        updatedAt: Date.now(),
-        ...video,
-      };
-
-      await indexedDB["history"].put(payload);
+      await indexedDB["history"].put(percentCompleted(playerRef.current));
     }, 2_000);
   };
 
@@ -46,21 +50,66 @@ export default function YoutubePlayer(
     }
   };
 
-  function _onStateChange(event: YT.OnStateChangeEvent) {
+  function getActivePlayers() {
+    return document.querySelectorAll("iframe");
+  }
+
+  async function _onStateChange(event: YT.OnStateChangeEvent) {
+    const { target, data: playingState } = event;
     const playerState = window.YT.PlayerState;
 
-    switch (event.data) {
-      case playerState.PLAYING:
-        startTracking();
-        break;
+    const allPlayers = getActivePlayers();
+
+    const filterPlayers = Array.from(allPlayers).filter(
+      (item) => item != target.getIframe()
+    );
+
+    console.log("playerS", playingState);
+
+    switch (playingState) {
+      case playerState.CUED:
       case playerState.PAUSED:
       case playerState.ENDED:
         stopTracking();
         break;
+
+      case playerState.PLAYING:
+        filterPlayers.forEach((item) => playerControl(item, "stopVideo"));
+        const playedVideo = await indexedDB["history"].get(video.videoId);
+        if (playedVideo) {
+          target.seekTo(playedVideo.duration, false);
+          target.playVideo();
+        }
+        startTracking();
+        break;
+      case playerState.BUFFERING:
+        await indexedDB["history"].put(percentCompleted(event.target));
+        console.log("Seeking");
+        break;
     }
   }
 
+  async function playerControl(
+    iframe: HTMLIFrameElement,
+    action: "playVideo" | "stopVideo" | "pauseVideo" | "destroy"
+  ) {
+    const payload = JSON.stringify({
+      args: [],
+      event: "command",
+      func: action,
+    });
+
+    iframe.contentWindow?.postMessage(payload, "*");
+  }
+
   async function loadIFrameElement() {
+    // Check if something is already playing, if so pause it and play the loaded one
+    // const allPlayers = getActivePlayers();
+
+    // allPlayers?.forEach((node) => {
+    //   playerControl(node, "pauseVideo");
+    // });
+
     if (!enableJsApi) {
       return;
     }
@@ -70,11 +119,11 @@ export default function YoutubePlayer(
         containerRef.current?.querySelector("lite-youtube") as any
       )?.getYTPlayer();
 
-      const playing = await indexedDB["history"].get(video.videoId);
+      // const playing = await indexedDB["history"].get(video.videoId);
 
-      if (playing?.videoId === video.videoId) {
-        playerRef.current?.seekTo(playing.duration, true);
-      }
+      // if (playing?.videoId === video.videoId) {
+      //   playerRef.current?.seekTo(playing.duration, true);
+      // }
 
       // Set up player event listeners
       playerRef.current?.addEventListener("onStateChange", _onStateChange);
